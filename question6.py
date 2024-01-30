2c571471a3534001098e78bf5e8c331c772fab32
ManjunathAineni <158119697+ManjunathAineni@users.noreply.github.com>
2024-01-29 23:27:57 -0500

    Update question6.py

--- question6.py
+++ question6.py
@@ -1,15 +1,21 @@
 def question6_1():
-    """
+    
     answer = {
-        'equal_width': ['string', integer],
-        'equal_frequency': ['string', integer]
+        'equal_width': ['No change', 9],
+        'equal_frequency': ['No change', 9]
     }
-    """
     return answer
 
 def question6_2():
+    answer = {
+        'equal_width': ['No change', 9],
+        'equal_frequency': ['No change', 9]
+    }
     return answer
 
 def question6_3():
-    return answer 
-
+    answer = {
+        'equal_width': ['change', 4],
+        'equal_frequency': ['No change', 9]
+    }
+    return answer
